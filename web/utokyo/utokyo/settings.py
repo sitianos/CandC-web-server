3bed1506b2a33c5c36e08d70b3f9c3f91d5f55c3
sitianos <sitianos@eidos.ic.i.u-tokyo.ac.jp>
2023-08-10 18:29:42 +0900

    add switch between mysql and sqlite3 fix some bugs

--- web/utokyo/utokyo/settings.py
+++ web/utokyo/utokyo/settings.py
@@ -11,12 +11,16 @@ ALLOWED_HOSTS = ['localhost', '.onion', os.getenv('SERVER_NAME')]
 LINE_TOKEN = os.getenv('LINE_TOKEN')
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME':  os.getenv('DB_NAME'),
+#        'USER': os.getenv('DB_USER'),
+#        'PASSWORD': os.getenv('DB_PASSWORD'),
+#        'HOST': os.getenv('DB_HOST'),
+#        'PORT': os.getenv('DB_PORT'),
+#    }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':  os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
